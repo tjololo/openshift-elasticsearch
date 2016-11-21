043fefc87266113a069b0af3269bf0ded923f228
Gaukstad, Vemund <vemund.gaukstad@brreg.no>
2016-11-21 15:33:28 +0100

    retry with --Xms=$(VALUE) instead of: -Xms$(VALUE)m

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,7 +6,7 @@ fi
 ES_HOME=/usr/share/elasticsearch
 DATA_DIR=/elasticsearch/storage/${ELASTICSEARCH_CLUSTER_NAME}/data
 WORK_DIR=/elasticsearch/${ELASTICSEARCH_CLUSTER_NAME}/work
-CONF_DIR=/etc/elasticsearch
+CONF_DIR=/opt/app-root/elasticsearch
 
 JAVA_OPTS=${JAVA_OPTS:-}
 ES_JAVA_OPTS="-Des.default.path.home=$ES_HOME -Des.default.path.data=$DATA_DIR -Des.default.path.work=$WORK_DIR -Des.default.path.conf=$CONF_DIR"
@@ -26,7 +26,7 @@ if [[ "${ELASTICSEARCH_MAX_MEMORY}" =~ $regex ]]; then
 		echo "ELASTICSEARCH_MAX_MEMORY set to ${ELASTICSEARCH_MAX_MEMORY} but must be at least 512M"
 		exit 1
 	fi
-	export ES_JAVA_OPTS+=" -Xms256M -Xmx$(($num/2))m"
+	export ES_JAVA_OPTS+=" --Xms=256M --Xmx=$(($num/2))m"
 else
 	echo "ELASTICSEARCH_MAX_MEMORY env var is invalid: ${ELASTICSEARCH_MAX_MEMORY}"
 	exit 1
